e1c1566f79e904b3737751abc07c12ee91f4546b
Julien Pinson <dinkertons@gmail.com>
2023-02-02 16:48:57 +0100

    Removed unused auth service in data-access.

--- src/app/shared/guards/auth/auth.guard.ts
+++ src/app/shared/guards/auth/auth.guard.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../../data-access/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
-    private authService: AuthService,
     private router: Router
   ) {}
 
@@ -16,17 +14,18 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const redirect = route.data['redirect'] as string || '';
+    /*const redirect = route.data['redirect'] as string || '';
     // requires to be authentified by default.
     const authentified = route.data['authentified'] ?? true;
 
-    const isAuthenticated = this.authService.isAuthenticated();
+    //const isAuthenticated = this.authService.isAuthenticated();
 
     if (
       (authentified && !isAuthenticated) ||
       (!authentified && isAuthenticated)
-    ) 
+    )
       return this.router.createUrlTree([redirect]);
+      */
 
     return true;
   }
